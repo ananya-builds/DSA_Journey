4e3c47a3a601713a22b9de13ae9565d6fd3963d1
ananya-builds <ananya.311912@gmail.com>
2026-08-09 00:03:01 +0530

    Patterns

--- Patterns/Pattern17.cpp
+++ Patterns/Pattern17.cpp
@@ -1,19 +1,35 @@
 #include <iostream>
 using namespace std;
 
+// Alternatively
 void print(int n){
     for (int i=0;i<n;i++){
         for (int j=1;j<n-i;j++){
             cout<<" ";
-        }for (int k=65;k<=65+i;k++){
-            cout<<char(k);
         }
-        for (int l=64+i;l>=65;l--){
-            cout<<char(l);
+        char ch='A';
+        int breakPoint = (2*i)/2;
+        for (int k=0;k<=2*i;k++){
+            cout<<ch;
+            if (k<breakPoint) ch++;
+            else ch--;
         }
         cout<<endl;
     }
 }
+// void print(int n){
+//     for (int i=0;i<n;i++){
+//         for (int j=1;j<n-i;j++){
+//             cout<<" ";
+//         }for (int k=65;k<=65+i;k++){
+//             cout<<char(k);
+//         }
+//         for (int l=64+i;l>=65;l--){
+//             cout<<char(l);
+//         }
+//         cout<<endl;
+//     }
+// }
 
 int main() {
     int n;
