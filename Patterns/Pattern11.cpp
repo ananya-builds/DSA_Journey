5b3419d1dbfb92e7d2ecfa4c77cbcdd10557d4c7
ananya-builds <ananya.311912@gmail.com>
2026-08-08 23:39:46 +0530

    Patterns

--- Patterns/Pattern11.cpp
+++ Patterns/Pattern11.cpp
@@ -1,26 +1,40 @@
 #include <iostream>
 using namespace std;
 
+// Alternatively
 void print(int n){
+    int start=0;
     for (int i=0; i<n; i++){
+        if (i%2==0) start=1;
+        else start=0;
         for (int j=0; j<=i; j++){
-            if (i%2==0){
-                if (j%2==0){
-                    cout<<"1";
-                }else{
-                    cout<<"0";
-                }
-            }else{
-                if (j%2==0){
-                    cout<<"0";
-                }else{
-                    cout<<"1";
-                }
-            }
+            cout<<start;
+            start=1-start;
         }
         cout<<endl;
     }
 }
+
+// void print(int n){
+//     for (int i=0; i<n; i++){
+//         for (int j=0; j<=i; j++){
+//             if (i%2==0){
+//                 if (j%2==0){
+//                     cout<<"1";
+//                 }else{
+//                     cout<<"0";
+//                 }
+//             }else{
+//                 if (j%2==0){
+//                     cout<<"0";
+//                 }else{
+//                     cout<<"1";
+//                 }
+//             }
+//         }
+//         cout<<endl;
+//     }
+// }
 
 int main() {
     int n;
